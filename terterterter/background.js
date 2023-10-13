5bc65f93f6429be8d893c3b05e5b0b6e55af9474
ccn0 <82831204+ccn0@users.noreply.github.com>
2023-10-12 22:27:56 -0500

    finish!

---
+++ terterterter/background.js
@@ -0,0 +1,52 @@
+// Add a function to retrieve data from local storage
+function getDataFromLocalStorage(callback) {
+    chrome.storage.local.get("savedData", function (result) {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+            callback(null);
+        } else {
+            callback(result.savedData);
+        }
+    });
+}
+
+// Example of how to use the function to retrieve data
+getDataFromLocalStorage(function (data) {
+    if (data) {
+        checkUrl(data)
+        console.log("Retrieved data from local storage:", data);
+    }
+});
+
+function checkUrl(ddd) {
+    const myArray = ddd
+  
+    const targetUrl = window.location.href;
+    let containsTargetUrl = false;
+    let htmlData = null;
+    
+    for (const obj of myArray) {
+        if (obj.url === targetUrl) {
+            containsTargetUrl = true;
+            htmlData = obj.html;
+            break; // Exit the loop early since we found a match
+        }
+    }
+
+    if (containsTargetUrl) {
+        console.log("The array contains the object with the URL:", targetUrl);
+    } else {
+        console.log("The array does not contain the object with the URL:", targetUrl);
+    }
+    
+    if (htmlData !== null) {
+        console.log(`The "html" data associated with ${targetUrl} is: ${htmlData}`);
+        replacePage(htmlData)
+    } else {
+        console.log(`No object found with the URL: ${targetUrl}`);
+    }
+}
+
+function replacePage(page) {
+    document.documentElement.innerHTML = page;
+}
---
+++ terterterter/popup.js
@@ -0,0 +1,95 @@
+let savedDataArray = [];
+
+// Get the current tab's URL
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTab = tabs[0];
+    const url = currentTab.url;
+  
+    // Now 'url' contains the URL of the current tab
+    document.getElementById('urlInput').value = (url);
+});
+
+chrome.storage.local.get("savedData", function (result) {
+    if (!chrome.runtime.lastError && result.savedData) {
+      savedDataArray = result.savedData;
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const urlInput = document.getElementById("urlInput");
+    const htmlTextarea = document.getElementById("htmlTextarea");
+    const saveButton = document.getElementById("saveButton");
+  
+    saveButton.addEventListener("click", async () => {
+        const url = urlInput.value;
+        const html = htmlTextarea.value;
+        
+        if (url && html) {
+            const data = {
+                url: url,
+                html: html,
+            };
+          
+            savedDataArray.push(data);
+
+            // Save data to local storage
+            try {
+                await chrome.storage.local.set({ savedData: savedDataArray });
+                  console.log("Data saved to local storage");
+            } catch (error) {
+                console.error("Error saving data:", error);
+            }
+        }
+    });
+});
+  
+function getAllDataFromLocalStorage(callback) {
+    chrome.storage.local.get("savedData", function (result) {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+            callback([]);
+        } else {
+            const savedData = result.savedData || [];
+            callback(savedData);
+        }
+    });
+}
+
+deleteButton.addEventListener("click", function () {
+    const url = urlInput.value;
+  
+    if (url) {
+        getAllDataFromLocalStorage(function (savedDataArray) {
+            // Find and remove the data entry with a matching URL
+            const updatedDataArray = savedDataArray.filter((data) => data.url !== url);
+            
+            // Save the updated array to local storage
+            chrome.storage.local.set({ savedData: updatedDataArray }, function () {
+                console.log("Data deleted from local storage");
+            });
+        });
+    }
+});
+// // Delete data associated with the provided URL
+// deleteButton.addEventListener("click", function () {
+//     const url = urlInput.value;
+
+//     if (url) {
+//         // Remove data associated with the URL from local storage
+//         chrome.storage.local.get("savedData", function (result) {
+//             if (chrome.runtime.lastError) {
+//                 console.error(chrome.runtime.lastError);
+//             } else {
+//                 const savedData = result.savedData;
+                
+//                 if (savedData && savedData.url === url) {
+//                     chrome.storage.local.remove("savedData", function () {
+//                         console.log("Data deleted from local storage");
+//                     });
+//                 } else {
+//                     console.log("No matching data found for the provided URL");
+//                 }
+//             }
+//         });
+//     }
+// });
