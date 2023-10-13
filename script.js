33c2897413227306ce02f4bb7844f40130242c15
ccn0 <82831204+ccn0@users.noreply.github.com>
2023-10-12 20:23:41 -0500

    dsfsa

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('HELLO WORLD    ')
+function init() {
+    const el = document.createElement("input")
+    el.setAttribute("type", "checkbox")
+    document.body.appendChild(el)
+    el.addEventListener("click", (event) => {
+        console.log(event.target.checked)
+    })
+}
+
+init()
